5feb0e7e05289159430c2e7e849d3c2b6d1875e8
Jesse Woo <woojesse@gmail.com>
2017-05-11 11:25:01 -0700

    Testing

--- express-authentication/test.sh
+++ express-authentication/test.sh
@@ -4,8 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 function test {
     cd "${DIR}/${1}";
+	pwd
     npm test;
     cd ${DIR};
+    pwd
 }
 
-test "converter";
+test "test";
