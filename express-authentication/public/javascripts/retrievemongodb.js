070d6553dbd3d16dbd507de5696b73449ef4b9eb
Jesse Woo <woojesse@gmail.com>
2017-07-01 00:52:28 -0700

    selection factory

--- express-authentication/public/javascripts/retrievemongodb.js
+++ express-authentication/public/javascripts/retrievemongodb.js
@@ -1,135 +1,148 @@
-$(function () {
-    $(document).on('click', '.submitRestButton', function ( event ) {
+// Retrieve MongoDB data for user
+$(document).ready(function () {
+    $("#saveUserQueries").hide();
+});
 
-        event.preventDefault();
-        var xmlText = $(this).attr("alt");
-        console.log(xmlText);
+$(document).on('click', '.submitRestButton', function ( event ) {
 
-        $("#xml_text_value").val(xmlText);
+    event.preventDefault();
+    var xmlText = $(this).attr("alt");
+    console.log(xmlText);
 
-        $('#submitForSearchResults').submit();
-    });
+    $("#xml_text_value").val(xmlText);
 
-    $(document).on('click', '#viewUserQueries', function () {
+    $('#submitForSearchResults').submit();
+});
 
-        var user_email = $('#user_email').html();
-        console.log(user_email);
+$(document).on('click', '#viewUserQueries', function () {
 
-        $.ajax({
-            type: "get",
-            url: "/searchquery/one/queries/email/" + user_email,
-            dataType: "json",
-            contentType: "application/json",
-            success: function (data) {
-                console.log("Retrieve Data from MongoDB for searchQueries table");
-                console.log(data);
+    var user_email = $('#user_email').html();
+    console.log(user_email);
 
-                // DATA - Loop thru each object
-                $.each(data[0]["search_queries"], function (index, object) {
-                    // Build HTML code for the table row
+    $.ajax({
+        type: "get",
+        url: "/searchquery/one/queries/email/" + user_email,
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            console.log("Retrieve Data from MongoDB for searchQueries table");
+            console.log(data);
 
-                    // console.log(object["query_xml"]);
-                    // var xmlText = new XMLSerializer().serializeToString(object["query_xml"]);
+            // DATA - Loop thru each object
+            $.each(data[0]["search_queries"], function (index, object) {
+                // Build HTML code for the table row
 
-                    // Query Description should be unique
+                // console.log(object["query_xml"]);
+                // var xmlText = new XMLSerializer().serializeToString(object["query_xml"]);
 
-                    var newRow = "<tr class='center'>";
-                    newRow += "<td>" + object["next_scheduled_run"] + "</td>";
-                    newRow += "<td>" + object["email_notification"] + "</td>";
-                    newRow += "<td>" + object["query_description"] + "</td>";
-                    newRow += "<td><input class='btn btn-primary submitRestButton' type='submit' value='Search' alt='" + object["query_xml"].toString() + "'></td>";
-                    newRow += "</tr>";
+                // Query Description should be unique
 
-                    $("#savedUserQueries > tbody:last-child").append(newRow);
-                });
-            },
-            error: function (err) {
-                console.log("ERROR: Unable to retrieve data!", err);
-            }
-        });
-    });
+                // Dropdown
 
-    // https://stackoverflow.com/questions/35050750/mongodb-creating-an-objectid-for-each-new-child-added-to-the-array-field
-    // DELETE sub object from main object
+                var newRow = "<tr class='center'>";
+                newRow += "<td>" + selectDropdownWorker(["monthly","weekly","none"],object["next_scheduled_run"]) + "</td>";
+                newRow += "<td>" + selectDropdownWorker(["true","false"],object["email_notification"]) + "</td>";
+                newRow += "<td>" + object["query_description"] + "</td>";
+                newRow += "<td><input class='btn btn-primary submitRestButton' type='submit' value='Search' alt='" + object["query_xml"].toString() + "'></td>";
+                newRow += "</tr>";
 
-    // Retrieve All Queries
-    $(document).on('click', '#viewAllUserQueries', function () {
-        $.ajax({
-            type: "get",
-            url: "/searchquery/all/queries",
-            dataType: "json",
-            contentType: "application/json",
-            success: function (data) {
-                console.log("Retrieve Data from MongoDB for ALL queries table");
-                console.log(data);
-
-                // DATA - Loop thru each object
-                $.each(data, function (index, object) {
-                    tableWorker(object);
-                });
-            },
-            error: function (err) {
-                console.log("ERROR: Unable to retrieve data!", err);
-            }
-        });
-    });
-
-
-    // Append to table function
-    var tableWorker = function (object) {
-        // Setup edit action field
-        var action_col = "<td><button type='button' class='btn btn-danger btn-xs delete-query' value='" + object._id + "'>Delete</button></td>";
-
-        // Build HTML code for the table row
-        var newRow = "<tr class='center'>";
-        newRow += "<td>" + object._id + "</td>";
-        newRow += "<td>" + object.email + "</td>";
-
-        // Go thru each object in the search queries ARRAY
-        object.search_queries.forEach( function(arrayItem) {
-            newRow += "<td>" + arrayItem["next_scheduled_run"] + "</td>";
-            newRow += "<td>" + arrayItem["email_notification"] + "</td>";
-            newRow += "<td>" + arrayItem["query_description"] + "</td>";
-
-            // http://www.rcsb.org/pdb/software/rest.do
-            console.log(arrayItem["query_xml"]);
-            newRow += "<td>" + arrayItem["query_xml"] + "</td>";
-        });
-
-        newRow += action_col;
-        newRow += "</tr>";
-
-        $("#savedAllUserQueries > tbody:last-child").append(newRow);
-    };
-
-
-    // Delete button click event listener
-    $(document).on('click', '.delete-query', function () {
-        var target = $(this);
-        // Send result to server for saving to object database
-        if (target.val()) {
-            var prep = {};
-            prep.id = target.val();
-            console.log("prep", prep);
-            var json = JSON.stringify(prep);
-            console.log('Requesting delete of ->', json);
-            $.ajax({
-                type: "delete",
-                url: "/searchquery/del/queries",
-                dataType: "json",
-                data: json,
-                contentType: "application/json",
-                success: function(data){
-                    console.log("Item removed from the database!");
-                    target.parent().parent().addClass('hide');
-                },
-                error: function(err){
-                    console.log("ERROR: Unable remove the item from the database!", err);
-                }
+                $("#savedUserQueries > tbody:last-child").append(newRow);
             });
+        },
+        error: function (err) {
+            console.log("ERROR: Unable to retrieve data!", err);
         }
     });
 
-
+    $("#viewUserQueries").hide();
+    $("#saveUserQueries").show();
 });
 
+// Selection Dropdown
+var selectDropdownWorker = function (optionsArray, selectedValue) {
+    var selectDropdownFinal = "<div class='form-group'><select class='form-control input-sm'>";
+
+    optionsArray.forEach( function(s) {
+        selectDropdownFinal += "<option value='" + s + "'";
+        // Add "selected" if it matches value
+        if (s === selectedValue) {
+            selectDropdownFinal += " selected";
+        }
+        // Close the first option tag
+        selectDropdownFinal += ">" + s + "</option>";
+    } );
+
+    selectDropdownFinal += "</select></div>";
+    return selectDropdownFinal;
+};
+
+
+// https://stackoverflow.com/questions/35050750/mongodb-creating-an-objectid-for-each-new-child-added-to-the-array-field
+// DELETE sub object from main object
+
+// Retrieve All Queries
+$(document).on('click', '#viewAllUserQueries', function () {
+    $.ajax({
+        type: "get",
+        url: "/searchquery/all/queries",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            console.log("Retrieve Data from MongoDB for ALL queries table");
+            console.log(data);
+
+            // DATA - Loop thru each object
+            $.each(data, function (index, object) {
+                tableWorker(object);
+            });
+        },
+        error: function (err) {
+            console.log("ERROR: Unable to retrieve data!", err);
+        }
+    });
+});
+
+
+// Append to table function
+var tableWorker = function (object) {
+    // Setup edit action field
+    var action_col = "<td><button type='button' class='btn btn-danger btn-xs delete-query' value='" + object._id + "'>Delete</button></td>";
+
+    // Build HTML code for the table row
+    var newRow = "<tr class='center'>";
+    newRow += "<td>" + object._id + "</td>";
+    newRow += "<td>" + object.email + "</td>";
+
+    newRow += action_col;
+    newRow += "</tr>";
+
+    $("#savedAllUserQueries > tbody:last-child").append(newRow);
+};
+
+
+// Delete button click event listener
+$(document).on('click', '.delete-query', function () {
+    var target = $(this);
+    // Send result to server for saving to object database
+    if (target.val()) {
+        var prep = {};
+        prep.id = target.val();
+        console.log("prep", prep);
+        var json = JSON.stringify(prep);
+        console.log('Requesting delete of ->', json);
+        $.ajax({
+            type: "delete",
+            url: "/searchquery/del/queries",
+            dataType: "json",
+            data: json,
+            contentType: "application/json",
+            success: function(data){
+                console.log("Item removed from the database!");
+                target.parent().parent().addClass('hide');
+            },
+            error: function(err){
+                console.log("ERROR: Unable remove the item from the database!", err);
+            }
+        });
+    }
+});
